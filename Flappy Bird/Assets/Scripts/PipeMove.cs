f814eba949a7bf5a79a6e63c91d8fcf6196d730a
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-15 15:15:22 +0300

    Pipes and Environment

---
+++ Flappy Bird/Assets/Scripts/PipeMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/PipeSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawn : MonoBehaviour
+{
+    public GameObject pipe;
+
+    void Start()
+    {
+        StartCoroutine(CreatePipe());
+    }
+
+    IEnumerator CreatePipe()
+    {
+        //for initial
+        yield return new WaitForSeconds(2.5f);
+        
+        while(true)
+        {
+            Instantiate(pipe, new Vector3(pipe.transform.position.x, Random.Range(-0.4f, 0.4f), 0), Quaternion.identity);
+            yield return new WaitForSeconds(1.1f);
+        }
+    }
+}
