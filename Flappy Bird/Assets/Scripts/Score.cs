af290fd408f29a8cdfa3006acbda65471c35e379
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-15 15:50:04 +0300

    Checkpoints and score

---
+++ Flappy Bird/Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "checkpoint")
+        {
+            Debug.Log("Score!!");
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+}
