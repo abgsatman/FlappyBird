8e7586a4e0d8d7cfb86f3162473b031c218af478
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-15 14:20:55 +0300

    Jumping physics with animation

---
+++ Flappy Bird/Assets/Scripts/FlyingBird.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBird : MonoBehaviour
+{
+    public float velocity = 2.5f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
